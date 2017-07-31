fea6393dff7911de8ecb3a92694c05dd66c890b5
Grégory D'Angelo <dangelo.gregory@gmail.com>
2017-07-31 13:36:50 +0200

    feat: Create the Input class

--- miniflow.py
+++ miniflow.py
@@ -17,3 +17,15 @@ class Node(object):
         Compute the output value based on `inbound_nodes` and store the result in self.value.
         '''
         raise NotImplemented
+
+class Input(Node):
+    def __init__(self):
+        # An Input node has no inbound nodes, so no need to pass anything to the Node instantiator.
+        Node.__init__(self)
+
+    # NOTE: Input node is the only node where the value may be passed as an argument to forward().
+    # All other node implementations should get the value of the previous node from self.inbound_nodes
+    def forward(self, value=None):
+        # Overwrite the value if one is passed in.
+        if value is not None:
+            self.value = value
